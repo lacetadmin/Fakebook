1443c10db4937eed62d22c6e60c89af0d4e4ec62
tomcat <tomcat@ip-172-31-26-45.us-west-2.compute.internal>
2015-11-01 00:06:14 +0000

    Lacet Push from lacetadmin

---
+++ client/templates/Mixed_Elements/Mixed_Elements.js
@@ -0,0 +1,6 @@
+Template.Mixed_Elements.helpers({
+//Add Template Helpers Here
+});
+Template.Mixed_Elements.events({
+//Add Event Handlers Here
+});
---
+++ client/templates/Sign_Up/Sign_Up.js
@@ -0,0 +1,6 @@
+Template.Sign_Up.helpers({
+//Add Template Helpers Here
+});
+Template.Sign_Up.events({
+//Add Event Handlers Here
+});
